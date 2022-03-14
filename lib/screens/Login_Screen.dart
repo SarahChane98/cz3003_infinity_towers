c3b424e3776eda72993c209a33da72b08e8f07e5
snehaarajkmrr <75203723+snehaarajkmrr@users.noreply.github.com>
2022-03-14 11:03:03 +0800

    Added Firebase connection for users, created connection between login page and tile map page/manage towers page via 'Instructor' toggle

---
+++ lib/screens/Login_Screen.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cz3003_infinity_towers/constants/button.dart';
+import 'package:cz3003_infinity_towers/constants/constants.dart';
+import 'Home_Screen.dart';
+import 'Register_Screen.dart';
+import 'tile_map.dart';
+import 'manage_towers.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formkey = GlobalKey<FormState>();
+  final _auth = FirebaseAuth.instance;
+  String email = '';
+  String password = '';
+  bool isloading = false;
+  bool isSwitched = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: isloading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Form(
+              key: formkey,
+              child: AnnotatedRegion<SystemUiOverlayStyle>(
+                value: SystemUiOverlayStyle.light,
+                child: Stack(
+                  children: [
+                    Container(
+                      height: double.infinity,
+                      width: double.infinity,
+                      constraints: BoxConstraints.expand(),
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("assets/log_in_background.jpg"),
+                            fit: BoxFit.cover),
+                      ),
+                      child: SingleChildScrollView(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 25, vertical: 120),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Infinity Towers",
+                              style: TextStyle(
+                                  fontSize: 50,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SwitchListTile(
+                              title:Text("I'm a Instructor",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 25)),value: isSwitched,
+                              onChanged: (value) {
+                                setState(() {
+                                  isSwitched = value;
+                                });
+                              },
+                              activeTrackColor: Colors.orangeAccent,
+                              activeColor: Colors.deepOrange,),
+                            TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              onChanged: (value) {
+                                email = value;
+                              },
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return "Please enter Email";
+                                }
+                              },
+                              textAlign: TextAlign.center,
+                              decoration: kTextFieldDecoration.copyWith(
+                                hintText: 'Email',
+                                prefixIcon: Icon(
+                                  Icons.email,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 30),
+                            TextFormField(
+                              obscureText: true,
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return "Please enter Password";
+                                }
+                              },
+                              onChanged: (value) {
+                                password = value;
+                              },
+                              textAlign: TextAlign.center,
+                              decoration: kTextFieldDecoration.copyWith(
+                                  hintText: 'Password',
+                                  prefixIcon: Icon(
+                                    Icons.lock,
+                                    color: Colors.black,
+                                  )),
+                            ),
+                            SizedBox(height: 80),
+                            LoginSignupButton(
+                              title: 'Login',
+                              ontapp: () async {
+                                if (formkey.currentState.validate()) {
+                                  setState(() {
+                                    isloading = true;
+                                  });
+                                  try {
+                                    await _auth.signInWithEmailAndPassword(
+                                        email: email, password: password);
+                                    if (isSwitched) {
+                                      await Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                          builder: (context) {
+                                            return ManageTowers();
+                                          },
+                                        ),
+                                      );
+                                    } else {
+                                      await Navigator.of(context).push(
+                                        MaterialPageRoute(
+                                          builder: (context) {
+                                            return TileMapPage();
+                                          },
+                                        ),
+                                      );
+                                    }
+                                    await Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (contex) => TileMapPage(),
+                                      ),
+                                    );
+
+                                    setState(() {
+                                      isloading = false;
+                                    });
+                                  } on FirebaseAuthException catch (e) {
+                                    showDialog(
+                                      context: context,
+                                      builder: (ctx) => AlertDialog(
+                                        title: Text("Ops! Login Failed"),
+                                        content: Text('${e.message}'),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(ctx).pop();
+                                            },
+                                            child: Text('Okay'),
+                                          )
+                                        ],
+                                      ),
+                                    );
+                                    print(e);
+                                  }
+                                  setState(() {
+                                    isloading = false;
+                                  });
+                                }
+                              },
+                            ),
+                            SizedBox(height: 30),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => SignupScreen(),
+                                  ),
+                                );
+                              },
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "Don't have an Account ?",
+                                    style: TextStyle(
+                                        fontSize: 20, color: Colors.black87),
+                                  ),
+                                  SizedBox(width: 10),
+                                  Hero(
+                                    tag: '1',
+                                                                      child: Text(
+                                      'Sign up',
+                                      style: TextStyle(
+                                          fontSize: 21,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+    );
+  }
+}
