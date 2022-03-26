84ae9a46c97f559b06175c823eda5314ea749c98
Chen Xinyi <xchen032@e.ntu.edu.sg>
2022-03-27 01:57:59 +0800

    Add leaderboard.

---
+++ lib/screens/leaderboard_tile_map.dart
@@ -0,0 +1,110 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cz3003_infinity_towers/constants/sizes.dart';
+import 'package:cz3003_infinity_towers/models/tower.dart';
+import 'package:cz3003_infinity_towers/models/tower_participation.dart';
+import 'package:cz3003_infinity_towers/screens/tower_leaderboard.dart';
+
+class LeaderboardTileMapPage extends StatefulWidget {
+  const LeaderboardTileMapPage({Key key}) : super(key: key);
+
+  @override
+  State<LeaderboardTileMapPage> createState() => _LeaderboardTileMapPageState();
+}
+
+class _LeaderboardTileMapPageState extends State<LeaderboardTileMapPage> {
+
+  Widget towerImage = Image.asset('assets/tower.png', width: 50, height: 50);
+  final towerParticipationsRef = FirebaseFirestore.instance.collection('tower_participations').withConverter<TowerParticipation>(
+    fromFirestore: (snapshot, _) => TowerParticipation.fromJson(snapshot.data()),
+    toFirestore: (participation, _) => participation.toJson(),
+  );
+
+  final towerRef = FirebaseFirestore.instance.collection('towers').withConverter<Tower>(
+    fromFirestore: (snapshot, _) => Tower.fromJson(snapshot.data()),
+    toFirestore: (tower, _) => tower.toJson(),
+  );
+
+  Future<List<QueryDocumentSnapshot<TowerParticipation>>> getParticipations() async {
+    var participatedTowers = await towerParticipationsRef
+        .where('studentId', isEqualTo: FirebaseAuth.instance.currentUser.uid)
+        .get()
+        .then((snapshot) => snapshot.docs);
+    return participatedTowers;
+  }
+
+  Future<List<Tower>> getTowers() async {
+    List<Tower> towers = [];
+    await getParticipations().then((participations) async {
+      for (var querySnapshot in participations) {
+        String towerId = querySnapshot.data().towerId;
+        await towerRef.doc(towerId).get().then((snapshot) => snapshot.data())
+            .then((tower) => towers.add(tower));
+      }
+    });
+    return towers;
+  }
+
+  Future<List<String>> getTowerIds() async {
+    List<String> towerIds = [];
+    await getParticipations().then((participations) async {
+      for (var querySnapshot in participations) {
+        String towerId = querySnapshot.data().towerId;
+        towerIds.add(towerId);
+      }
+    });
+    return towerIds;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Select a tower", textAlign: TextAlign.center),
+        ),
+        body: FutureBuilder(
+            future: Future.wait([getTowers(), getTowerIds()]),
+            builder: (BuildContext context, snapshot) {
+              if (snapshot.hasError) {
+                return Text("Something went wrong");
+              }
+
+              if (!snapshot.hasData) {
+                return Text("Document does not exist");
+              }
+
+              if (snapshot.connectionState == ConnectionState.done) {
+                List<Tower> towers = snapshot.data[0];
+                List<String> towerIds = snapshot.data[1];
+                return ListView.separated(
+                  padding: const EdgeInsets.all(Sizes.smallPadding),
+                  itemCount: towers.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      height: Sizes.towerTileHeight,
+                      child: ListTile(
+                        leading: towerImage,
+                        title: Text('Tower: ${towers[index].name}'),
+                        trailing: Icon(Icons.navigate_next),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TowerLeaderboardPage(towerId: towerIds[index]),
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) => const Divider(),
+                );
+              }
+
+              return Text("Loading");
+            }
+        )
+    );
+  }
+}
---
+++ lib/screens/tower_leaderboard.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:cz3003_infinity_towers/models/tower_participation.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cz3003_infinity_towers/constants/sizes.dart';
+import 'dart:collection';
+
+class TowerLeaderboardPage extends StatelessWidget {
+  TowerLeaderboardPage({Key key, @required this.towerId}) : super(key: key);
+
+  final String towerId;
+  final towerParticipationsRef = FirebaseFirestore.instance.collection('tower_participations').withConverter<TowerParticipation>(
+    fromFirestore: (snapshot, _) => TowerParticipation.fromJson(snapshot.data()),
+    toFirestore: (participation, _) => participation.toJson(),
+  );
+
+  Future<String> getUserEmail(String uid) async {
+    CollectionReference usersRef = FirebaseFirestore.instance.collection('users');
+    String userEmail;
+    await usersRef.where('uid', isEqualTo: uid).get().then((value) {
+      userEmail = (value.docs[0].data() as Map<String, dynamic>)['email'];
+    });
+    return userEmail;
+  }
+
+  Future<Map<String, int>> getAllUserScores() async {
+    Map<String, int> allUserScores = {};
+    await towerParticipationsRef
+      .where('towerId', isEqualTo: towerId)
+      .get()
+      .then((snapshot) => snapshot.docs)
+      .then((participations) async {
+        for (var querySnapshot  in participations) {
+          var userId = querySnapshot.data().studentId;
+          await getUserEmail(userId).then((email) {
+            allUserScores[email] = querySnapshot.data().score;
+          });
+        }
+    });
+    return allUserScores;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Leaderboard", textAlign: TextAlign.center),
+        ),
+        body: FutureBuilder(
+            future: getAllUserScores(),
+            builder: (BuildContext context, snapshot) {
+              if (snapshot.hasError) {
+                return Text("Something went wrong");
+              }
+
+              if (!snapshot.hasData) {
+                return Text("Document does not exist");
+              }
+
+              if (snapshot.connectionState == ConnectionState.done) {
+                Map<String, int> userScores = snapshot.data;
+                final sortedScores = SplayTreeMap.from(
+                    userScores, (key2, key1) => userScores[key1].compareTo(userScores[key2]));
+                var emails = sortedScores.keys.toList();
+                var scores = sortedScores.values.toList();
+                return ListView.separated(
+                  padding: const EdgeInsets.all(Sizes.smallPadding),
+                  itemCount: scores.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      height: Sizes.towerTileHeight,
+                      child: ListTile(
+                        title: Text(emails[index]),
+                        trailing: Text(scores[index].toString()),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) => const Divider(),
+                );
+              }
+              return Text("Loading");
+            }
+        )
+    );
+  }
+}
